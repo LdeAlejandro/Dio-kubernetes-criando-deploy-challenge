14919e1f948e0b885d9cb39a7f0624943d0c5da8
LdeAlejandro <letzalet@gmail.com>
2025-03-04 00:37:53 +0000

    fixing writing animation files

--- animation.sh
+++ animation.sh
@@ -1,25 +1,19 @@
+
 #!/bin/bash
 
 typewriter() {
-  text="$1"
-  length=${#text}
-  
-  for ((i=0; i<length; i++)); do
-    char="${text:$i:1}"
-    
-    if [[ "$char" == " " ]]; then
-      printf " "
-    else
-      printf "$char"
-    fi
-    
-    # Controle de tempo - ajuste o valor para mudar a velocidade
-    # 0.001 = muito rápido, 0.01 = rápido, 0.1 = normal
-    sleep 0.005
-  done
-  
-  echo ""
+    local text="$1"
+    local i=0
+    while [ $i -lt ${#text} ]; do
+        char="${text:$i:1}"
+        if [ "$char" == " " ]; then
+            echo -n " "  # Preserve spaces
+        else
+            echo -n "$char"
+        fi
+        sleep 0.0025  # Speed adjustment
+        ((i++))
+    done
+    echo  # New line at the end
 }
 
-# Chamada da função com o primeiro argumento passado
-typewriter "$1"
