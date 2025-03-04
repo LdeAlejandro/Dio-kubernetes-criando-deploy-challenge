439461de7f91c6305b3b4eaee14405af73170489
Alejandro Amoroso <letzalet@gmail.com>
2025-03-03 23:24:09 -0300

    desafio completado

--- frontend/js.js
+++ frontend/js.js
@@ -5,7 +5,7 @@ $("#button-blue").on("click", function() {
     var txt_comentario = $("#comment").val();
 
     $.ajax({
-        url: "http://172.18.252.57:30706",
+        url: "http://172.18.252.57:30706/",
         
         type: "post",
         data: {nome: txt_nome, comentario: txt_comentario, email: txt_email},
