17ba425b08ddae5cd1536bc2394e9314958fc293
LdeAlejandro <letzalet@gmail.com>
2025-03-04 00:47:58 +0000

    correcting bat

--- script.bat
+++ script.bat
@@ -7,7 +7,7 @@ docker build -t ldealejandro/k8-challlenge-backend:1.0 backend/.
 docker build -t ldealejandro/k8-challlenge-database:1.0 database/.
 
 
-call animation.bat "Realizando o push das imagens 
+call animation.bat "Realizando o push das imagens" 
 
 :: Carregamento das imagens para o dockerhub
 docker push ldealejandro/k8-challlenge-backend:1.0
